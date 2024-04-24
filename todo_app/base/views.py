9e2134e73fb49a3260e75521c5f330f6b37017e6
markohautala <markohautala@live.se>
2024-04-24 21:49:25 +0200

    Added detail view for tasks

--- todo_app/base/views.py
+++ todo_app/base/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import Task
 
 # Create your views here.
 class TaskList(ListView):
     model = Task
     context_object_name = 'todo_tasks'
+
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = 'todo_task'
+    template_name = 'base/single_task.html'
