3962175e1056216ee1781ee322d9f1b61971a012
markohautala <markohautala@live.se>
2024-04-24 21:56:37 +0200

    Add CreateTask view for task creation

--- todo_app/base/views.py
+++ todo_app/base/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
 from .models import Task
 
 # Create your views here.
@@ -12,3 +13,6 @@ class TaskDetail(DetailView):
     model = Task
     context_object_name = 'todo_task'
     template_name = 'base/single_task.html'
+
+class CreateTask(CreateView):
+    
