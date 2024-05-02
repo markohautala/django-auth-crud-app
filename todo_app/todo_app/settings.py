aa853c1e48b1ddbb6ea6488a632502380afe8b42
markohautala <markohautala@live.se>
2024-05-02 21:47:21 +0200

    Add whitenoise middleware and storage for static files

--- todo_app/todo_app/settings.py
+++ todo_app/todo_app/settings.py
@@ -51,7 +51,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'todo_app.urls'
 
