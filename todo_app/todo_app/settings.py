e5b3f29b387091bdfc189abddde12c12f2b1d0b2
markohautala <markohautala@live.se>
2024-05-02 22:38:43 +0200

    Move runtime.txt and Procfile to root directory

--- todo_app/todo_app/settings.py
+++ todo_app/todo_app/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-81u25$q$3i8!sfrkz&_=b&xz8ar8*$m@sy!*n6)-22!&*h*p9s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['django-crud-auth-application-2e5e251b2b30.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
@@ -125,11 +125,10 @@ LOGIN_URL = 'login'
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+
 django_heroku.settings(locals())
 
 # Default primary key field type
